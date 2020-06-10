908fa10b45a5324415b15a81eb9a2a95b21a557d
mingchun.or <mingchuno@gmail.com>
2020-06-11 00:06:35 +0800

    add url and more way to parse text

--- tutorial/spiders/gnimmm_spider.py
+++ tutorial/spiders/gnimmm_spider.py
@@ -5,13 +5,21 @@ class GnimmmSpider(scrapy.Spider):
     start_urls = [
         'https://gnimmm.com/2020/05/25/fagara/',
     ]
-    def parse(self, response):
+
+    def get_content(self, response):
         contents = response.css('.post > div.entry-content > p::text').getall()
-        print(contents)
+        result = ' '.join(contents)
+        if not result:
+            contents = response.css('.post > div.entry-content > p > span::text').getall()
+            result = ' '.join(contents)
+        return result
+
+    def parse(self, response):
         yield {
             'datetime': response.css('.post > header > div > a.posted-on > time.entry-date.published::attr(datetime)').get(),
             'title': response.css('.post > header > h1 > span::text').get(),
-            'text': ' '.join(contents)
+            'text': self.get_content(response),
+            'url': response.url
         }
         next_page = response.css('#main > nav > div > div > a::attr(href)').get()
         if next_page is not None:
